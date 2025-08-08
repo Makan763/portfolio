381c015515b9111055fcd1b9b6777ba6c75cb775
Makan763 <makandiombana2@gmail.com>
2025-08-08 16:45:52 +0200

    Adding portfolio files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <main class="flex-fill">
+      <router-outlet></router-outlet>
+    </main>
+    <app-footer></app-footer>
+  `
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent {}
